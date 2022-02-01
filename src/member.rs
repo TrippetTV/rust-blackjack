df5d1c5d8928a3dabd00a83073bd3fd3940213ec
Trippet <erik.sjolinswahn@gmail.com>
2022-02-01 13:42:05 +0100

    Added more nuance to formatting, starting to be complete with dealer behavior, player behavior still TODO

--- src/member.rs
+++ src/member.rs
@@ -11,21 +11,43 @@ pub enum Member {
 
 impl fmt::Display for Member {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}s Turn", self)
+        match self {
+            Member::Dealer(dealer) => {
+                write!(f, "{}'s Turn", dealer.player.name)
+            }
+            Member::Player(player) => {
+                write!(
+                    f,
+                    "{}s Turn, Score: {},  Hand: {:?}",
+                    player.name, player.score, player.hand
+                )
+            }
+        }
     }
 }
 
 impl Member {
-    pub(crate) fn turn(&mut self, ctx: &mut Deck) -> () {
+    pub(crate) fn turn(&mut self, ctx: &mut Deck) -> usize {
         match self {
             Member::Dealer(dealer) => {
-                if dealer.player.score >= 17 {
-                    dealer.player.pass()
+                return if dealer.player.score >= 17 {
+                    dealer.player.pass();
+                    1
                 } else {
-                    dealer.player.hit(ctx)
+                    dealer.player.hit(ctx);
+                    0
                 }
-            } // See if need to hit, hit until self.dealer.score >= 17
-            Member::Player(player) => {} // Hit or Pass :)
+            }
+            Member::Player(player) => {
+                //TODO implement the AI to calculate the odds of winning and determine behaviour accordingly.
+                return if player.score <= 15 {
+                    player.hit(ctx);
+                    0
+                } else {
+                    player.pass();
+                    1
+                };
+            }
         }
     }
 }
