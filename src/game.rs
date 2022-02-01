1ac3d9bb6fb14f8e699cb614e7359c6f7028a41b
Trippet <erik.sjolinswahn@gmail.com>
2022-02-01 13:43:54 +0100

    Implemented starting the deal of cards

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,8 @@
+use crate::member::Member;
 use crate::playerlist::PlayerList;
 use crate::Deck;
 
-///GameHandler handles the game
+///GameHandler handles the game.
 #[derive(Debug)]
 pub struct GameHandler {
     pub(crate) current_game: Game,
@@ -13,17 +14,19 @@ impl GameHandler {
             current_game: Game::new(),
         };
     }
-    /// Starts a new game with new players
+    /// Starts a new game with new players.
     pub(crate) fn start(&mut self) {
         self.current_game.set_table();
+        self.current_game.deal_cards();
         self.current_game.play_game();
     }
-    /// Cleans "the table"
+    /// Cleans "the table".
     pub(crate) fn end(&self) {
         todo!("End Game not yet implemented")
     }
 }
-/// Game is the players and the deck
+
+/// Game is the players and the deck.
 #[derive(Debug)]
 pub struct Game {
     pub(crate) deck: Deck,
@@ -37,15 +40,45 @@ impl Game {
             players: PlayerList::new(),
         };
     }
-    /// "Sets the table", i.e. shuffles the deck, and resets the players
+
+    /// "Sets the table", i.e. shuffles the deck, and resets the players.
     fn set_table(&mut self) {
         self.deck = Deck::new();
         self.deck.deck_shuffle();
         self.players = PlayerList::new();
     }
+
     ///
-    // TODO move to GameHandler, probably
+    pub(crate) fn deal_cards(&mut self) {
+        for i in 0..2 {
+            for member in &mut self.players.players {
+                match member {
+                    Member::Dealer(dealer) => dealer.player.hit(&mut self.deck),
+                    Member::Player(player) => player.hit(&mut self.deck),
+                }
+            }
+        }
+    }
+
+    /// Plays the game until and the end is reached.
     fn play_game(&mut self) {
-        self.players.turn(&mut self.deck);
+        loop {
+            if let Member::Dealer(dealer) = self.players.current_member() {
+                if self.dealer_is_done() {
+                    println!("Dealer score: {}", dealer.player.score);
+                    break;
+                }
+            }
+            self.players.turn(&mut self.deck);
+        }
+    }
+    fn dealer_is_done(&self) -> bool {
+        return match self.players.current_member() {
+            Member::Dealer(dealer) => {
+                dealer.player.score >= 17
+                    && self.players.current_index == self.players.players.len()
+            }
+            Member::Player(_) => false,
+        };
     }
 }
