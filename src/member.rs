43e36c248166c73800cfea16b4096b2442d4cb29
Trippet <erik.sjolinswahn@gmail.com>
2022-02-16 14:25:15 +0100

    AI correctly implemented it into member turns.

--- src/member.rs
+++ src/member.rs
@@ -1,3 +1,4 @@
+use crate::ai::AI;
 use crate::dealer::Dealer;
 use crate::player::Player;
 use crate::Deck;
@@ -13,41 +14,49 @@ impl fmt::Display for Member {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Member::Dealer(dealer) => {
-                write!(f, "{}'s Turn", dealer.player.name)
-            }
-            Member::Player(player) => {
                 write!(
                     f,
-                    "{}s Turn, Score: {},  Hand: {:?}",
-                    player.name, player.score, player.hand
+                    "{}'s Turn, Score: {}",
+                    dealer.player.name, dealer.player.score
                 )
+            }
+            Member::Player(player) => {
+                write!(f, "{}s Turn, Score: {}", player.name, player.score)
             }
         }
     }
 }
 
 impl Member {
+    /// Turn behaviour
     pub(crate) fn turn(&mut self, ctx: &mut Deck) -> usize {
-        match self {
+        // Return if hit or pass
+        return match self {
             Member::Dealer(dealer) => {
-                return if dealer.player.score >= 17 {
-                    dealer.player.pass();
+                if dealer.player.score >= 17 {
+                    dealer.player.pass(false);
                     1
                 } else {
-                    dealer.player.hit(ctx);
+                    dealer.player.hit(ctx, false);
                     0
                 }
             }
             Member::Player(player) => {
-                //TODO implement the AI to calculate the odds of winning and determine behaviour accordingly.
-                return if player.score <= 15 {
-                    player.hit(ctx);
+                let ai: AI = AI {
+                    deck: ctx.clone(),
+                    hand: (*player.hand.to_vec()).to_owned(),
+                };
+
+                let odds = ai.odds().floor();
+                if odds >= 50.0 {
+                    println!("{}: The odds are in my favor! {}%", player.name, odds);
+                    player.hit(ctx, false);
                     0
                 } else {
-                    player.pass();
+                    player.pass(false);
                     1
-                };
+                }
             }
-        }
+        };
     }
 }
