cc14f3efe51d5e0b6dd067ce6a5c60a2c9890c79
Trippet <erik.sjolinswahn@gmail.com>
2022-02-17 15:26:26 +0100

    Comment Update

--- src/deck.rs
+++ src/deck.rs
@@ -5,7 +5,9 @@ use std::fmt;
 use strum::IntoEnumIterator;
 
 #[derive(Debug, Clone)]
+/// A list of cards.
 pub struct Deck {
+    /// a vector of cards
     pub(crate) cards: Vec<Card>,
 }
 
@@ -19,15 +21,15 @@ impl Deck {
     /// Create a new Deck with a full list of sorted cards.
     pub(crate) fn new(amount: u8) -> Deck {
         let mut card_list: Vec<Card> = vec![];
-
-        for i in 0..amount {
+        // Amount of decks to be in play
+        for _i in 0..amount {
+            // For each unique card, add that card to the deck,
             for suit in Suit::iter() {
                 for value in Value::iter() {
                     card_list.push(Card { value, suit });
                 }
             }
         }
-
         return Deck { cards: card_list };
     }
     /// Shuffle the cards of called deck using a crate called SliceRandom
