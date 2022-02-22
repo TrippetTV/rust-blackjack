4cadcb98d2de593cf33547649f40119481109041
Trippet <erik.sjolinswahn@gmail.com>
2022-02-22 09:02:07 +0100

    Changed the file from a struct to just a public method with parameters.

--- src/ai.rs
+++ src/ai.rs
@@ -1,33 +1,24 @@
 use crate::{Card, Deck};
 
-pub(crate) struct AI {
-    pub(crate) deck: Deck,
-    pub(crate) hand: Vec<Card>,
-}
+/// Gets the odds of hitting a card in the deck without going over 21 depending on any given hand.
+pub fn odds(ctx: &mut Deck, hand: &Vec<Card>) -> f32 {
+    // Gets the score sum of the current players hand
+    let score = hand
+        .clone()
+        .into_iter()
+        .map(|x| x.value.get_number())
+        .sum::<u8>();
 
-impl AI {
-    /// Gets the odds of hitting a card in the deck without going over 21 depending on any given hand.
-    pub fn odds(&self) -> f32 {
-        // Gets the score sum of the current players hand
-        let score = &self
-            .hand
-            .clone()
-            .into_iter()
-            .map(|x| x.value.get_number())
-            .sum::<u8>();
+    // Count each time a card in deck when drawn + score <= 21
+    let count = ctx
+        .cards
+        .iter()
+        .filter(|c| c.value.get_number() + score <= 21)
+        .count();
 
-        // Count each time a card in deck when drawn + score <= 21
-        let count = self
-            .deck
-            .cards
-            .iter()
-            .filter(|c| c.value.get_number() + score <= 21)
-            .count();
+    // Gives the odds of hitting a card in the deck that doesn't go over 21
+    let math = (count as f32) / (ctx.cards.iter().count() as f32) * 100.0;
 
-        // Gives the odds of hitting a card in the deck that doesn't go over 21
-        let math = (count as f32) / (self.deck.cards.iter().count() as f32) * 100.0;
-
-        // Return the calculated odds
-        return math;
-    }
+    // Return the calculated odds
+    return math;
 }
